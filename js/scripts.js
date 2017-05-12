571f0ef72803e4f7aca355733fe265c63f0d2e2a
LenaKuchko <kuchkoel@gmail.com>
2017-05-12 09:58:33 -0700

    add calculating for whole order price

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,26 @@
 function Pizzeria(name, address) {
   this.name = name;
   this.orders = [];
-}
+};
 
-function Order(customerName, price) {
+function Order(customerName) {
   this.customerName = customerName;
-  this.price = price;
   this.orderItems = [];
-}
+  this.orderPrice = 0;
+};
 
 function Pizza(size, dough) {
   this.size = size;
   this.dough = dough;
-  this.basePrice = 5;
+  this.basicPrice = 5;
   this.toppings = [];
-}
+  this.finalPrice;
+};
+
 function Topping(name, price) {
   this.name = name;
   this.price = price;
-}
+};
 
 Pizza.prototype.addTopping = function (name, price) {
   var topping = new Topping(name, price);
@@ -27,35 +29,69 @@ Pizza.prototype.addTopping = function (name, price) {
 };
 
 Pizza.prototype.calculatePrice = function () {
-  var pizzaPrice = this.basePrice;
+  this.finalPrice = this.basicPrice;
   for (var i = 0; i < this.toppings.length; i++) {
-    pizzaPrice+=this.toppings[i].price;
+    this.finalPrice+=this.toppings[i].price;
     console.log(this.toppings[i]);
-    console.log(pizzaPrice);
+    console.log(this.finalPrice);
   }
   if (this.size === "medium") {
-    pizzaPrice+=1;
+    this.finalPrice+=1;
   } else if (this.size === "large") {
-    pizzaPrice+=3;
+    this.finalPrice+=3;
   } else if (this.size === "family") {
-    pizzaPrice+=5;
+    this.finalPrice+=5;
   } else {
-    pizzaPrice+=0;
+    this.finalPrice+=0;
   }
-  
+
   if (this.dough === "thin") {
-    pizzaPrice+=2;
+    this.finalPrice+=2;
   } else if (this.dough === "original") {
-    pizzaPrice+=4;
+    this.finalPrice+=4;
   } else if (this.dough === "gluten-free") {
-    pizzaPrice+=5;
+    this.finalPrice+=5;
   }
-  return pizzaPrice;
+  return this.finalPrice;
 };
 
-var pizza = new Pizza (15, 2);
+Order.prototype.addOrderItem = function (orderItem) {
+  this.orderItems.push(orderItem);
+};
+
+Order.prototype.calculateOrderPrice = function () {
+     for (var i = 0; i < this.orderItems.length; i++) {
+       this.orderPrice+=this.orderItems[i].finalPrice;
+     }
+  return this.orderPrice;
+};
+
+Pizzeria.prototype.addOrder = function (order) {
+  this.orders.push(order);
+};
+
+$(function () {
+  $("#form").submit(function (event) {
+    event.preventDefault();
+
+  });
+});
+
+var shop = new Pizzeria ("myPizzeria", "2139 W.Burnside");
+
+
+var pizza = new Pizza ("thin", "family");
 
 pizza.addTopping("cheese", 2);
 pizza.addTopping("qwe", 4);
 pizza.addTopping("rty", 6);
 pizza.calculatePrice();
+
+var order = new Order ("Larry");
+
+order.addOrderItem(pizza);
+order.addOrderItem(pizza);
+
+order.calculateOrderPrice();
+
+ console.log(order);
